a765e49c9c62f86c0946bec108134939baa20b66
Filip Hauptfleisch <Filip.hauptfleisch@gmail.com>
2022-09-17 12:13:27 +0200

    Add starting push and update assets

--- Assets/Scripts/Flying/PlaneObject.cs
+++ Assets/Scripts/Flying/PlaneObject.cs
@@ -4,49 +4,32 @@ using UnityEngine;
 
 public class PlaneObject : MonoBehaviour
 {
-    [SerializeField] Rigidbody _rigidbody;
-    [SerializeField] float _underwaterDrag = 2.0f;
-    [SerializeField] float _underwaterAngularDrag = 5.0f;
-    private PhysicsAffector[] _physicsAffectors;
-    private EngineAffector[] _engineAffectors;
-    public PhysicsAffector[] physicsAffectors => _physicsAffectors;
-    private PlanePhysicsData _planePhysicsData;
+    [SerializeField] PlanePhysicsFlightController _planePhysicsFlightController;
+    [SerializeField] PlaneStartPushController _planeStartPushController;
+    [SerializeField] float _startPushDuration;
 
-    private float _forcesMultiplier = 1;
+    private bool _startingPushInProgress = false;
+    private float _pushStartTime;
 
-    void Start()
+    protected void Start()
     {
-        _physicsAffectors = GetComponentsInChildren<PhysicsAffector>();
-        _engineAffectors = GetComponentsInChildren<EngineAffector>();
+        StartPush();
     }
 
-    void Update()
+    protected void Update()
     {
-        ProcessFlight();
-    }
-
-    private void ProcessFlight()
-    {
-        ApplyForcesForAffectors();
-        ApplyForcesForEngines();
-    }
-
-    private void ApplyForcesForAffectors()
-    {
-        foreach(var affector in _physicsAffectors)
+        if (_startingPushInProgress)
         {
-            //Add gravity
-            _rigidbody.AddForceAtPosition(_forcesMultiplier * affector.weight * Physics.gravity, affector.position);
-            //Add resistance
-            _rigidbody.AddForceAtPosition(affector.transform.rotation * (_forcesMultiplier * affector.resistance * Vector3.up), affector.position);
+            _planeStartPushController.ProcessFlight();
+            if ((Time.time - _pushStartTime) >= _startPushDuration)
+            {
+                _startingPushInProgress = false;
+                Debug.Log("Initial push ended");
+            }
         }
-    }
-
-    private void ApplyForcesForEngines()
-    {
-        foreach(var engineAffector in _engineAffectors)
+        else
         {
-            _rigidbody.AddForceAtPosition(engineAffector.centerTransform.rotation * (_forcesMultiplier * engineAffector.engineSrength * engineAffector.engineThrustDirection), engineAffector.centerTransform.position);
+            _planePhysicsFlightController.ProcessFlight();
         }
     }
 
@@ -55,9 +38,14 @@ public class PlaneObject : MonoBehaviour
         var waterLayerMask = LayerMask.GetMask("Water");
         if (waterLayerMask == (waterLayerMask | (1 << other.gameObject.layer)))
         {
-            _forcesMultiplier = 1f;
-            _rigidbody.drag = _underwaterDrag;
-            _rigidbody.angularDrag = _underwaterAngularDrag;
+            _planePhysicsFlightController.SetWaterMultipliers();
         }
     }
+
+    public void StartPush()
+    {
+        _pushStartTime = Time.time;
+        _startingPushInProgress = true;
+    }
+
 }
