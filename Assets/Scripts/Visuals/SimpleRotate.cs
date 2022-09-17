a9c7324d4bd69a624208f4530364b66db199faad
jackbeatgames <jackbeatgames@fb.com>
2022-09-17 17:03:32 +0200

    improved scene visuals

---
+++ Assets/Scripts/Visuals/SimpleRotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float _rotateSpeed;
+    public Vector3 _rotateDir;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(_rotateSpeed * _rotateDir * Time.deltaTime);
+    }
+}
